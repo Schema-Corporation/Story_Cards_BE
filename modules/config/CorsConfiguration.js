8e841677ae70afb5418c646085207d48549e17b9
Alvaro Toconas <alvarotoconas@indigital.pe>
2021-04-02 12:52:10 -0500

    feat: #60 Added all origins and explicit front end origin to cors configuration

--- modules/config/CorsConfiguration.js
+++ modules/config/CorsConfiguration.js
@@ -2,6 +2,8 @@ const server = {
     PORT: 3000,
     NAME: "STORYCARDS BACKEND",
     CLIENTS: [
+        "*",
+        "http://storycards.dev.s3-website-us-west-2.amazonaws.com/",
         "http://storycardsfront.eba-3w7npwge.us-west-2.elasticbeanstalk.com/",
         "http://localhost:8100",
         "http://storycards.upc.edu.pe",
